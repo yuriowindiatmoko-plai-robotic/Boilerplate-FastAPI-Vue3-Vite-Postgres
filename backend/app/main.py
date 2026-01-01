b44f64c948d5ddfa8a75ecc17c9be535587007f4
yuriowindiatmoko-plai-robotic <yuriowindiatmoko@plai.ac.id>
2026-01-01 20:05:52 +0700

    cool now frontend-vite already works with integration signup or login to HomeView

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://localhost:5174"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
